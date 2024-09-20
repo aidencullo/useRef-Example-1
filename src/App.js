dc0ecf287166b756828fe38c4e90c90e9123f0fd
Aiden Cullo <aidencullo@gmail.com>
2024-09-20 12:10:44 -0400

    controlled form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import { useState, useRef } from 'react';
+import { useState } from "react"
 
-export default function Counter() {
-  const [show, setShow] = useState(true);
-  const ref = useRef(null);
+const Form = props => {
+  // State to hold the form data.
+  const [form, setForm] = useState({
+    name: "",
+    age: 0,
+  })
 
+  // handleChange function
+  const handleChange = event => {
+    // Dynamically update the state using the event object.
+    // This function always looks the same.
+    setForm({ ...form, [event.target.name]: event.target.value })
+  }
+
+  const handleSubmit = event => {
+    // Prevent page refresh on submission.
+    event.preventDefault()
+
+    // Do what you want with the form data.
+    console.log(form)
+  }
+
+  // The JSX for the form, binding the functions and state to our inputs.
   return (
-    <div>
-      <button
-        onClick={() => {
-          setShow(!show);
-        }}>
-        Toggle with setState
-      </button>
-      <button
-        onClick={() => {
-          ref.current.remove();
-        }}>
-        Remove from the DOM
-      </button>
-      {show && <p ref={ref}>Hello world</p>}
-    </div>
-  );
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={form.name}
+        onChange={handleChange}
+        name="name"
+        placeholder="Your Name"
+      />
+      <input
+        type="number"
+        value={form.age}
+        onChange={handleChange}
+        name="age"
+        placeholder="Your Age"
+      />
+      <input type="submit" value="Submit Form" />
+    </form>
+  )
 }
